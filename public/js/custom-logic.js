b8c9e7c9eda72e9b1925989cc74b9ba2137adee8
Lorenzo Turrino <neuronefunzionante@gmail.com>
2019-03-08 23:40:07 +0000

    implement front-end call to delete post

--- public/js/custom-logic.js
+++ public/js/custom-logic.js
@@ -1,1 +1,16 @@
-console.log('hello custom logic!')
+const deletePost = postIndex => {
+    const fullDeleteUrl = `/post/${postIndex}`;
+
+    fetch(fullDeleteUrl, {
+        method: "DELETE"
+    }).then(res => {
+        // console.log('all done', res);
+        location.reload();
+    })
+}
+
+const deleteButtons = document.querySelectorAll('.delete-post');
+
+deleteButtons.forEach(element => {
+    element.onclick = () => deletePost(element.dataset.index);
+});
