bbcb6ea1c248e934f67dd362955d661816a8add3
Lorenzo Turrino <neuronefunzionante@gmail.com>
2019-03-08 20:40:04 +0000

    install bodyparser, create post route, console.log to ensure correct parsing

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
 
 const blogPosts = require("./data/blogPosts.json")
 
 const app = express();
+const bodyParserMiddleware = bodyParser.urlencoded({extended: true})
 
 app.engine("handlebars", handlebars());
 app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
+app.use(bodyParserMiddleware);
 
 app.get("/", (req, res) => {
   res.render("index", {
@@ -34,6 +37,11 @@ app.get("/post/:postId", (req, res) => {
 app.get("/composepost", (req, res) => {
   res.render("compose-post");
 });
+
+app.post("/post", (req, res) => {
+  console.log(req.body)
+  res.send('received')
+})
 
 const SERVER_PORT = process.env.PORT || 3000;
 app.listen(SERVER_PORT, function() {
